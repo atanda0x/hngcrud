74e7f80b1cb51326a51a4b3c46220174293e5895
atanda0x <atandanafiu21@gmail.com>
2024-05-08 15:04:56 -0700

    middleware

---
+++ chapter3/log/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	log.Println("Processing request!")
+	w.Write([]byte("OK\n"))
+	log.Println("Finished processing request")
+}
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handle)
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	http.ListenAndServe(":9000", loggedRouter)
+}
