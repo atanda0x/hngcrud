3b71527b4333d7125464bb817a1f40c6f3692c39
atanda0x <atandanafiu21@gmail.com>
2024-05-14 16:11:42 -0700

    restful example

--- chapter4/restfulExample/main.go
+++ chapter4/restfulExample/main.go
@@ -1,18 +1,25 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"io"
-// 	"time"
-// )
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
 
-// func pingTime(r *restful.Request, w *restful.Response)  {
-// 	io.WriteString(w, fmt.Sprintf("%s", time.Now()))
-// }
+	"github.com/emicklei/go-restful"
+)
 
-// func main()  {
-// 	// create a web service
-// 	webService := new(restful.WebService)
+func pingTime(req *restful.Request, resp *restful.Response) {
+	// Write to the response
+	io.WriteString(resp, fmt.Sprintf("%s\n", time.Now()))
+}
 
-// 	//
-// }
+func main() {
+	// Create a web service
+	webservice := new(restful.WebService)
+	// Create a route and attach it to handler in the service
+	webservice.Route(webservice.GET("/ping").To(pingTime))
+	// Add the service to application
+	restful.Add(webservice)
+	http.ListenAndServe(":8000", nil)
+}
