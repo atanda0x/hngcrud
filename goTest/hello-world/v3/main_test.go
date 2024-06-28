b8b0c7b87720118e77053b447ecce3ff262b5890
atanda0x <atandanafiu21@gmail.com>
2024-06-28 13:38:51 -0700

    added assertMessaage to v3

--- goTest/hello-world/v3/main_test.go
+++ goTest/hello-world/v3/main_test.go
@@ -7,17 +7,20 @@ func Test(t *testing.T) {
 		got := Hello("atanda0x")
 		want := "Hello, atanda0x"
 
-		if got != want {
-			t.Errorf("got %q but want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello  World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q but want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
 }
