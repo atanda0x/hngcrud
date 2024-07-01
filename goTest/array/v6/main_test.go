eb8c02d1e49f56dd375b3e9c2e7d61ed847c1744
atanda0x <atandanafiu21@gmail.com>
2024-07-01 16:44:31 -0700

    v6 slice refactoored

--- goTest/array/v6/main_test.go
+++ goTest/array/v6/main_test.go
@@ -6,21 +6,25 @@ import (
 )
 
 func TestSumAllTails(t *testing.T) {
+
+	checkSum := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v but want %v", got, want)
+		}
+	}
+
 	t.Run("Make the sum of some slices", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v but want %v", got, want)
-		}
+		checkSum(t, got, want)
 
 		t.Run("Safety sum empty slices", func(t *testing.T) {
 			got := SumAllTails([]int{}, []int{3, 4, 5})
 			want := []int{0, 9}
 
-			if !reflect.DeepEqual(got, want) {
-				t.Errorf("got %v but want %v", got, want)
-			}
+			checkSum(t, got, want)
 		})
 	})
 
